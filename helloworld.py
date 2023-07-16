c7ebc17cde74418ea42afed67e5267cc374b0e72
zerokii <z3rocolt@gmail.com>
2023-07-16 12:24:57 +0800

    meeting 1

--- helloworld.py
+++ helloworld.py
@@ -1,3 +1,11 @@
-print("helloworld")
-print(12+3)
-print("12+3")
+print("""name:dennis
+      age:17
+      hobby:watching movies and gaming""")
+print(9+7)
+print(96-37)
+print(96*2)
+print(87/4)
+print(87%4)
+#%=remainder
+print(87//4)
+#//=floor operation
