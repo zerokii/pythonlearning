cfd50c7167223ec34df2daf62ca42513ff44b234
zerokii <z3rocolt@gmail.com>
2023-07-30 11:54:48 +0800

    oppenheimer

---
+++ meeting2.py
@@ -0,0 +1,8 @@
+# Steak Restaurant
+# Welcome your guests, U have a general welcome message
+print('Welcome to Dennis Grills')
+# Ask for their order
+order=input('What would you like to have today?\n')
+# Reply back with the order and price
+price=300
+print(f"The price for the {order} is ${price}. Thank you and please come again.")
---
+++ meeting3.py
@@ -0,0 +1,17 @@
+#for i in range (-10, -21, -1):
+#    print(i)
+
+#h = 85
+#while h<96:
+#    print(h)
+#    h+=1
+
+for r in range (5):
+    for j in range (r+1):
+        print("*", end=" ")
+    print("")
+
+for r in range (4):
+    for j in range (4, r, -1):
+        print("*", end=" ")
+    print("")
